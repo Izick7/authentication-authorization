3c2589d339c9a170faeb737edfc376b7edbcd34e
Izick7 <zickox491@gmail.com>
2026-09-20 15:08:15 +0100

    Refactor hashedPassword

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,12 @@
 const bcrypt = require('bcryptjs');
-const users = require('../data/users'); 
+const users = require('../data/users');
 const jwt = require('jsonwebtoken')
 
 
 // registaration logic
 const register = async (req, res) => {
     try {
-        const { name, email, password, role} = req.body;
+        const { name, email, password, role } = req.body;
 
         if (!name || !email || !password || !role) {
             return res.status(400).json({ message: "All fields are required" });
@@ -14,25 +14,28 @@ const register = async (req, res) => {
 
         const userExists = users.find(user => user.email === email);
         if (userExists) {
-            return res.status(400).json({ message: "Email already registered" });
+            return res.status(400).json({
+                message
+                    : "Email already registered"
+            });
         }
 
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
+        
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         const newUser = {
             id: users.length + 1,
             name,
             email,
-            password: hashedPassword, 
-            role: role.toLowerCase()  
+            password: hashedPassword,
+            role: role.toLowerCase()
         };
 
         users.push(newUser);
 
         res.status(201).json({
             message: "User registered successfully",
-            user: { id: newUser.id, name: newUser.name, email: newUser.email, role: newUser.role, password: hashedPassword}
+            user: { id: newUser.id, name: newUser.name, email: newUser.email, role: newUser.role, password: hashedPassword }
         });
 
     } catch (error) {
@@ -60,12 +63,12 @@ const login = async (req, res) => {
         }
 
         const token = jwt.sign(
-            { id: user.id, role: user.role }, 
-            "SUPER_SECRET_KEY_123", 
-            { expiresIn: "1h" } 
+            { id: user.id, role: user.role },
+            "SUPER_SECRET_KEY_123",
+            { expiresIn: "1h" }
         );
 
-      
+
         res.status(200).json({
             message: "Login successful",
             token: token
